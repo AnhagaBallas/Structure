b008b9c9d79d5fd3c8f338eb3c35d69a66cd96ec
AnhagaBallas <mickle1212@mail.ru>
2023-04-16 19:14:28 +0300

    Adding files

---
+++ Fasade/src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps binOps = new BinOps();
+        System.out.println(binOps.sum("010101010", "10010101010"));
+        System.out.println(binOps.mult("1010101001", "010101010"));
+    }
+}
